f82dd915407abab9845f854fac02f9aa90413bcc
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-12-21 18:11:23 -0300

    component nav criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+import { NavComponent } from './components/nav/nav.component';
+
 /*
 // Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
@@ -51,7 +53,8 @@ import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
