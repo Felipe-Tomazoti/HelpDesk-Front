c5e5efda768a0f3aaf6a58c40049c490bbbabb78
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-12-22 12:48:28 -0300

    component HEADER criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatCardModule } from '@angular/material/card';
 
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 /*
 // Componentes do projeto
@@ -56,7 +57,8 @@ import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
