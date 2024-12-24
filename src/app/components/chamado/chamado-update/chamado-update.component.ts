923fbba3196142a7c186ad1938bb4387c5d2ab17
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-12-24 12:08:05 -0300

    servico para atualizar chamado implementado

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+
 
 @Component({
   selector: 'app-chamado-update',
@@ -7,9 +17,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoUpdateComponent implements OnInit {
 
-  constructor() { }
+  chamado: Chamado = {
+      prioridade: '',
+      status: '',
+      titulo: '',
+      observacoes: '',
+      tecnico: '',
+      cliente: '',
+      nomeCliente: '',
+      nomeTecnico: ''
+    }
+  
+    clientes: Cliente[] = [];
+    tecnicos: Tecnico[] = [];
+  
+    prioridade: FormControl = new FormControl(null, [Validators.required]);
+    status: FormControl = new FormControl(null, [Validators.required]);
+    titulo: FormControl = new FormControl(null, [Validators.required]);
+    observacoes: FormControl = new FormControl(null, [Validators.required]);
+    tecnico: FormControl = new FormControl(null, [Validators.required]);
+    cliente: FormControl = new FormControl(null, [Validators.required]);
+  
+  
+    constructor(
+      private chamadoService: ChamadoService,
+      private clienteService: ClienteService,
+      private tecnicoService: TecnicoService,
+      private toastService: ToastrService,
+      private router: Router,
+      private route: ActivatedRoute
+    ) { }
+  
+    ngOnInit(): void {
+      this.findAllClientes();
+      this.findAllTecnicos();
+      this.chamado.id = this.route.snapshot.paramMap.get('id');
+      this.findById();
+    }
+    
+    findById(): void{
+      this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+        this.chamado = resposta;
+      })
+    }
 
-  ngOnInit(): void {
+    update(): void{
+      this.chamadoService.update(this.chamado).subscribe(resposta => {
+        this.toastService.success('Chamado atualizado com sucesso', 'Update');
+        this.router.navigate(['chamados']);
+      }, ex => {
+        if(ex.error.errors) {
+          ex.error.errors.forEach(element => {
+            this.toastService.error(element.message);
+          })
+        } else {
+          this.toastService.error(ex.message);
+        }
+      })
+    }
+
+    findAllClientes(): void {
+      this.clienteService.findAll().subscribe(resposta => {
+        this.clientes = resposta;
+      })
+    }
+  
+    findAllTecnicos(): void {
+      this.tecnicoService.findAll().subscribe(resposta => {
+        this.tecnicos = resposta;
+      })
+    }
+  
+    validaCampos(): boolean {
+      return this.prioridade.valid &&
+        this.status.valid &&
+        this.titulo.valid &&
+        this.observacoes.valid &&
+        this.tecnico.valid &&
+        this.cliente.valid
+    }
+
+    retornaStatus(status: any): string {
+      if (status == '0') {
+        return 'ABERTO'
+      } else if (status == '1') {
+        return 'EM ANDAMENTO'
+      } else {
+        return 'ENCERRADO'
+      }
+    }
+  
+    retornaPrioridade(prioridade: any): string {
+      if (prioridade == '0') {
+        return 'BAIXA'
+      } else if (prioridade == '1') {
+        return 'MÉDIA'
+      } else {
+        return 'ALTA'
+      }
+    }
+  
   }
-
-}
